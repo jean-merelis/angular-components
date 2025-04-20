486c623dfba8bd29588e6fd680bab3da422f3656
Jeandeson O. Merelis <jean.merelis@gmail.com>
2025-04-20 13:42:28 -0300

    fix(showcase)

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { SelectShowcaseComponent } from './components/select-showcase/select-showcase.component';
 import { ProgressBarShowcaseComponent } from './components/progress-bar-showcase/progress-bar-showcase.component';
-import { SelectComponent } from "./select/select.component";
+import { SelectShowcaseComponent } from './components/select-showcase/select-showcase.component';
+
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'select', component: SelectShowcaseComponent },
-    { path: 'select2', component: SelectComponent },
-    { path: 'progress-bar', component: ProgressBarShowcaseComponent },
-    { path: '**', redirectTo: '' }
+    {path: '', component: HomeComponent},
+    {path: 'select', component: SelectShowcaseComponent},
+    {path: 'progress-bar', component: ProgressBarShowcaseComponent},
+    {path: '**', redirectTo: ''}
 ];
